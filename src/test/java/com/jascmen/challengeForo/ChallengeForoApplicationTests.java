1a8a1b94bffe38b87a5ce5ec6c2a45c50b08c574
Jorge Anthony Sanchez Chavez <jascmen@gmail.com>
2024-07-13 17:31:03 -0500

    first commit

---
+++ src/test/java/com/jascmen/challengeForo/ChallengeForoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jascmen.challengeForo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeForoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
